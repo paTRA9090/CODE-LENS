3a0708be725c983ee615691c2df31a0b2e405cf5
patraRahul5678 <kanhapatra801@gmail.com>
2025-12-13 17:11:52 +0530

    hh

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,9 +15,12 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -26,13 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
